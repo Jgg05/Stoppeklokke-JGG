29370ac5c6fe1f5220a5a26a3501b03bda344030
Jgg05 <jonas.gjul@gmail.com>
2023-03-29 09:40:57 +0200

    Add project files.

---
+++ Stoppeklokke JGG/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Stoppeklokke_JGG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BttnStart = new System.Windows.Forms.Button();
+            this.BttnStop = new System.Windows.Forms.Button();
+            this.BttnReset = new System.Windows.Forms.Button();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // BttnStart
+            // 
+            this.BttnStart.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BttnStart.Font = new System.Drawing.Font("Product Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BttnStart.Location = new System.Drawing.Point(100, 325);
+            this.BttnStart.Name = "BttnStart";
+            this.BttnStart.Size = new System.Drawing.Size(172, 66);
+            this.BttnStart.TabIndex = 0;
+            this.BttnStart.Text = "Start";
+            this.BttnStart.UseVisualStyleBackColor = false;
+            this.BttnStart.Click += new System.EventHandler(this.BttnStart_Click);
+            // 
+            // BttnStop
+            // 
+            this.BttnStop.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BttnStop.Font = new System.Drawing.Font("Product Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BttnStop.Location = new System.Drawing.Point(278, 325);
+            this.BttnStop.Name = "BttnStop";
+            this.BttnStop.Size = new System.Drawing.Size(172, 66);
+            this.BttnStop.TabIndex = 1;
+            this.BttnStop.Text = "Stop";
+            this.BttnStop.UseVisualStyleBackColor = false;
+            this.BttnStop.Click += new System.EventHandler(this.BttnStop_Click);
+            // 
+            // BttnReset
+            // 
+            this.BttnReset.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BttnReset.Font = new System.Drawing.Font("Product Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BttnReset.Location = new System.Drawing.Point(467, 325);
+            this.BttnReset.Name = "BttnReset";
+            this.BttnReset.Size = new System.Drawing.Size(172, 66);
+            this.BttnReset.TabIndex = 2;
+            this.BttnReset.Text = "Reset";
+            this.BttnReset.UseVisualStyleBackColor = false;
+            this.BttnReset.Click += new System.EventHandler(this.BttnReset_Click);
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Product Sans", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHour.Location = new System.Drawing.Point(88, 94);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(174, 122);
+            this.lblHour.TabIndex = 3;
+            this.lblHour.Text = "00";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Product Sans", 75F);
+            this.lblMin.Location = new System.Drawing.Point(268, 90);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(182, 128);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "00";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Font = new System.Drawing.Font("Product Sans", 37F);
+            this.lblSec.Location = new System.Drawing.Point(456, 138);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(91, 63);
+            this.lblSec.TabIndex = 5;
+            this.lblSec.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Product Sans", 75F);
+            this.label1.Location = new System.Drawing.Point(227, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 128);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Product Sans", 37F);
+            this.label2.Location = new System.Drawing.Point(420, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 63);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.BttnReset);
+            this.Controls.Add(this.BttnStop);
+            this.Controls.Add(this.BttnStart);
+            this.Controls.Add(this.lblHour);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Stoppeklokke";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BttnStart;
+        private System.Windows.Forms.Button BttnStop;
+        private System.Windows.Forms.Button BttnReset;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Stoppeklokke JGG/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Stoppeklokke_JGG
+{
+    public partial class Form1 : Form
+    {
+
+        int timeSec;
+        int timeMin;
+        int timeHour;
+        bool isActive;
+        
+        //Om jeg trykker på "start" knappen vil isActive bli true.
+        private void BttnStart_Click(object sender, EventArgs e)
+        {
+            isActive = true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Når jeg starter programmet vil timeren bli resettet.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTime();
+
+            isActive = false;
+        }
+
+        //Når jeg trykker på stopknappen stopper timeren uten å bli resettet.
+        private void BttnStop_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+        }
+
+        //Når jeg trykker på "Reset" knappen vil timeren resette seg, den vil også stoppe.
+        private void BttnReset_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+
+            ResetTime();
+        }
+
+        //private void ResetTime() forteller hva ResetTime(); gjør.
+        private void ResetTime()
+        {
+            timeSec = 0;
+            timeMin = 0;
+            timeHour = 0;
+        }
+
+        //Her står det hva som skjer for hvert tick av timeren jeg satt inn i form1.cs[Design]
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //Her sier den at sekunder skal starte om "isActive" er true.
+            if (isActive)
+            {
+                timeSec++;
+
+                //Her sier den at om sekunder blir mer eller lik 60 vil et minutt bli lagt til.
+                if (timeSec >= 60)
+                {
+
+                    timeMin++;
+                    timeSec = 0;
+                }
+                //Her sier den at om minuttene blir mer eller lik 60 vil en time bli lagt til.
+                if (timeMin >= 60)
+                {
+                    timeHour++;
+                    timeMin = 0;
+                }
+            }
+            DrawTime();
+        }
+
+        //Her forteller "private void DrawTime()" hva "DrawTime()" gjør.
+        //Den forteller hvilket format tallene skal være.
+        private void DrawTime()
+        {
+            lblSec.Text = String.Format("{0,00}", timeSec);
+            lblMin.Text = String.Format("{0,00}", timeMin);
+            lblHour.Text = String.Format("{0,00}", timeHour);
+        }
+    }
+}
